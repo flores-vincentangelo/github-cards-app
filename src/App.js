1851a903bbd448c83b3692c967a7a3943b54ecb0
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2022-09-01 18:09:53 +0800

    updates

--- src/App.js
+++ src/App.js
@@ -7,22 +7,30 @@ export default class App extends React.Component {
 
     constructor(props) {
       super(props);
-      this.state = {};
+      this.state = {
+        profiles: [],
+      };
+    }
+
+    addNewProfile = (profileData) => {
+      this.setState(prevState => ({
+        profiles: [...prevState.profiles, profileData],
+      }));
     }
 
     render () {
       return (
         <>
             <div className='header'>{this.props.title}</div>
-            <Form />
-            <CardList profiles={testData}/>
+            <Form onSubmit={this.addNewProfile}/>
+            <CardList profiles={this.state.profiles}/>
         </>
       );
     }
 }
 
-const testData = [
-  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-];
+// const testData = [
+//   {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+//   {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+//   {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+// ];
--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,28 @@
 import React from 'react';
+// import fetch from 'node-fetch';
 
 export default class Form extends React.Component {
+    state = {userName: ''};
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        const response = await 
+            fetch(`https://api.github.com/users/${this.state.userName}`)
+            .then(body => body.json())
+
+        this.props.onSubmit(response);
+        this.setState({userName: ''})
+
+    };
     render () {
         return (
-            <form>
-                <input type="text" placeholder='GitHub username'/>
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                    type="text" 
+                    placeholder='GitHub username' 
+                    value={this.state.userName}
+                    onChange={event => this.setState({ userName: event.target.value })}
+                    required
+                />
                 <button>Add card</button>
             </form>
         );
